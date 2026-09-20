1252f55bc5adaf04afc100421dfc1b25d12e4244
jtiagotoledo <jtiagotoledo@gmail.com>
2026-09-20 14:41:48 -0300

    consulta basica de ticker

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,37 @@
-import { View, Text, TouchableHighlight } from 'react-native';
+import { View, Text, TouchableHighlight, TextInput } from 'react-native';
+import { useState } from 'react';
 import obterCotacao from '../services/brapiService'
+
 export default function HomeScreen() {
 
-  const pegarCotacao = async () =>{
-    const dados = await obterCotacao('BPAC11');
-    console.log('pegarCotação funcionando', dados.results[0].data);
-    
+  const [ticker, setTicker] = useState('');
+  const [naoExisteTicker, setNaoExisteTicker] = useState(false);
+
+  const pegarCotacao = async (ticker:string) => {
+    setNaoExisteTicker(false)
+    const dados = await obterCotacao(ticker);
+    if(!dados) setNaoExisteTicker(true);
+    console.log('dados', dados);
   }
+
   return (
     <View className="flex-1 items-center justify-center bg-zinc-100">
       <Text className="text-xl font-bold text-cyan-500">
-        Caronte Invest!  
+        Caronte Invest! 
       </Text>
-      <TouchableHighlight onPress={pegarCotacao}>
+      <TextInput
+        className='border mt-4 text-center'
+        placeholder='Digite o tiker aqui'
+        onChangeText={(text)=>setTicker(text)}
+      />
+      <TouchableHighlight onPress={()=>pegarCotacao(ticker)}>
         <Text className='bg-zinc-400 rounded-md p-2 text-zinc-100 mt-4'>
-          Pegar Cotação
+          Pegar Cotação 
         </Text>
       </TouchableHighlight>
+      {naoExisteTicker && 
+        <Text className='text-cyan-500 mt-4'>Ativo não existe</Text>
+      }
     </View>
   );
 }
--- src/services/brapiService.ts
+++ src/services/brapiService.ts
@@ -3,16 +3,23 @@ export default async function obterCotacao(ticker: string) {
     const url = `https://brapi.dev/api/v2/stocks/quote?symbols=${ticker}`
 
     try {
-        const response = await fetch(url,{
-            headers:{
+        const response = await fetch(url, {
+            headers: {
                 Authorization: `Bearer ${token}`,
             }
         });
-        const data = response.json();
-        return data;
-        
-        
-    } catch (error) {
 
+        if (response.status === 404) return null;
+        if (!response.ok) return null;
+
+        const dados = await response.json();
+        if (dados.results && dados.results.length > 0) {
+            return dados.results[0];
+        }
+
+        return null;
+    } catch (error) {
+        console.log('Erro', error);
+        return null;
     }
 }
